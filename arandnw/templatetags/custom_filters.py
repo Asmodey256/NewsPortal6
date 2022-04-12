1b914b5f6251391a7caefeb99bc54c7a16cde8d5
Asmodey256 <97547000+Asmodey256@users.noreply.github.com>
2022-04-12 20:13:07 +0300

    Add files via upload

---
+++ arandnw/templatetags/custom_filters.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+WORDS = ['лесом','нафнаф', 'big', 'sometitle']
+
+@register.filter(name='Censor')
+def Censor(value):
+    if not isinstance(value, str):
+        raise ValueError('Нельзя цензурировать не строку')
+
+    for word in WORDS:
+        value = value.replace(word[1:], '*' * (len(word) - 1))
+    return value
